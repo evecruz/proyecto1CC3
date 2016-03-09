3659985272a1f93022671609be418bfc631568d5
evecruz <evecruz@galileo.edu>
2016-03-08 19:45:47 -0800

    Update calc_depth.c

--- calc_depth.c
+++ calc_depth.c
@@ -21,9 +21,60 @@ unsigned char normalized_displacement(int dx, int dy,
 
 }
 
+int pixVal(int x, int y, int feature_width, int feature_height, int image_width, int image_height){
+	if [((x-feature_width)<0)|| ((y-feature_height<0))]{ //revisa que el feature no se salga del lado izquierdo
+		return 0;
+	}else{
+		return 1;
+	}
+
+	if [((x+feature_width)< image_width) && ((y+feature_height)< image_height)]{ // revisa que el feature no se salga del lado derecho
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+int cantFeatures(int maximum_displacement){
+	return ((2*maximum_displacement +1)*(2*maximum_displacement +1));
+}
+
+
+int distance(int feature_height, int feature_width){
+
+}
+
+
+
+
 void calc_depth(unsigned char *depth_map, unsigned char *left,
                 unsigned char *right, int image_width, int image_height,
                 int feature_width, int feature_height, int maximum_displacement) {
+unsigned char cuadritoIzq* = malloc((2 * feature_width + 1) * (2* feature_height + 1));// Creamos el cuadrito del lado izq
+unsigned char desplazamiento;
+unsigned char vecCuadrito*;
+int i,j;
+int 
+   for(i =0;i<image_width,i++){                //recorremos la imagen izq derecha arriba abajo.
+      for(j = 0;j<image_height;j++){
+	 	       cuadritoIzq[]
+        int pos = 0;
+        for(x=0;x<feature_width;x++){
+	 	      for(y=0;y<feature_height;y++){
+	 	   	   cuadritoIzq[pos]=left[x*feature_width+y];//paso a vector el cuadrito
+           pos++;
+	 	   }
+          	}
+          	desplazamiento=normalized_displacement(x,y,maximum_displacement);
+          	for(ii=0;ii<maximum_displacement;ii++){
+          		for(jj=0;jj<maximum_displacement;jj++){
+          			//llamar a pixVal para saber si es valido el pixel
+          		}
+          	}
+         }
+         	
+         }
 
-	/* YOU CODE HERE*/
+
 }
